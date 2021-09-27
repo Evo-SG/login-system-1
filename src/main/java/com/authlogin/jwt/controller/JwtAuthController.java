31b8c583cd5febb92a59d2bedcaeede2a9188f33
Jinghao1209 <84082781+Jinghao1209@users.noreply.github.com>
2021-09-27 12:15:15 +0800

    add @CrossOrigin("*")

--- src/main/java/com/authlogin/jwt/controller/JwtAuthController.java
+++ src/main/java/com/authlogin/jwt/controller/JwtAuthController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class JwtAuthController {
 	@Autowired
     private JwtUserSevice userService;
 
+    @CrossOrigin("*")
     @PostMapping(value="/authenticate")
     @ResponseBody
     public Map<String,Object> authLogin(String username,String password){
